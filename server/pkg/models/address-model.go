7cb1217c23fb15764280d5dddf27c81143e57728
Eric <erictoribio2010@gmail.com>
2022-07-18 19:09:21 -0400

    CHECK IF ADDRESS EXIST FUNC

--- server/pkg/models/address-model.go
+++ server/pkg/models/address-model.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+)
 
 type Address struct {
 	gorm.Model
@@ -8,4 +11,21 @@ type Address struct {
 	City   string `json:"city"`
 	State  string `json:"state"`
 	Zip    string `json:"zip"`
+}
+
+func (a *Address) CreateAddress() *Address {
+	db.NewRecord(a)
+	db.Create(a)
+	return a
+}
+
+func AddressExists(address Address) (*Address, error) {
+	var PossibleAddress Address
+	db.Find(&PossibleAddress, "street = ? AND city = ? AND state = ? AND zip = ?", address.Street, address.City, address.State, address.Zip)
+	if PossibleAddress.Street == "" {
+		err := errors.New("no address")
+		return &PossibleAddress, err
+	}
+	return &PossibleAddress, errors.New("nil")
+
 }
